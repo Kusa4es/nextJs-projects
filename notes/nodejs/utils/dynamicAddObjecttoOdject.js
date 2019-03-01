8931e2e22685dc3e8aca695a25f9f450488ba0d6
Evgeniy <javadev6891@gmail.com>
2019-03-01 11:22:48 +0300

    or

--- notes/nodejs/utils/dynamicAddObjecttoOdject.js
+++ notes/nodejs/utils/dynamicAddObjecttoOdject.js
@@ -9,3 +9,38 @@ for(let i = 0; i < arr.length; i++){
     ret[arr[i].key]=arr[i].value;
 }
 console.log(ret)
+
+
+
+// OR
+
+{
+    let arr  = [{key:"ID", value:"sdf324324"},{}, {key:"Key In", value:"sdfee"}, {key:"process Definition", value:"step 1"}];
+
+    let ret = {};
+    let orQueries = [];
+    let query = {
+    orQueries
+    };
+
+    for(let i = 0; i < arr.length; i++){
+    if(arr[i].key === undefined && arr[i].value === undefined){
+    continue;
+    }
+    ret[arr[i].key]=arr[i].value;
+    }
+
+    let isAny = true
+
+    isAny ? query = ret : orQueries.push(ret);
+
+    let body = {
+    resourceType: "Task",
+    name: "namesome",
+    owner: "jonny1",
+    query
+    }
+
+
+    console.log(body)
+}
